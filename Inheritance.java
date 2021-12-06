26703d9d76fd547fbe8917b3224ae7c8535e17de
Choi_J_I <95062692+Shaharen@users.noreply.github.com>
2021-12-06 16:40:44 +0900

    Add files via upload

---
+++ Inheritance.java
@@ -0,0 +1,50 @@
+package music;
+
+import 상속01.Folder;
+import 상속01.Tell;
+
+public class Inheritance {
+
+	public static void main(String[] args) {
+		// 상속(Inheritance)
+		// 기존 클래스(부모)의 변수와 메소드를 물려받아 새로운 클래스(자식) 구성
+		
+//		// 상위(부모) 클래스
+//		public void Call() {
+//			System.out.println("전화를 걸다");
+//		}
+//		public void CallSend() {
+//			System.out.println("전화를 받다");
+//		}
+//		public void ButtonClick() {
+//			System.out.println("버튼을 돌려서 선택하다");
+//		}
+//
+//		// 상속받을 하위 클래스
+//		
+//		// 확장 -> 필요한 기능만 추가로 개발
+//		public void Camera() {
+//			System.out.println("사진을 찍다");
+//		}
+//		// 오버라이딩 -> 재정의
+//		@Override // 어노테이션 참조
+//		public void ButtonClick() {
+//			System.out.println("똑 똑 똑 버튼을 클릭합니다");
+//		}
+//// 메인
+//		public static void main(String[] args) {
+//			
+//			Tell tell = new Tell();
+//			tell.Call();
+//			tell.ButtonClick();
+//			// 부모에 있는 기능을 전부 상속 받았기 때문에
+//			// 작성해주지 않아도 기능 사용이 가능하다
+//			Folder folder = new Folder();
+//			folder.Call();
+//			folder.Camera();
+//			folder.ButtonClick();
+//		}
+//
+	}
+
+}
